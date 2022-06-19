9b559196bf7e8ccd4571407aed51a742b6418cb3
Alexander Bidenko <alexbidenko1998@gmail.com>
2022-06-19 14:36:50 +0300

    Fix

--- plugins/userInfo.js
+++ plugins/userInfo.js
@@ -7,6 +7,12 @@ export default async ({ store, req }) => {
     const response = await fetch(`http://www.geoplugin.net/json.gp?ip=${ip}`)
     const data = await response.json()
     console.log('ClientIp:', ip, data)
+    console.log(
+      'Test2:',
+      req.headers,
+      req.headers['x-forwarded-for'],
+      req.socket.remoteAddress
+    )
     store.commit(
       'UserModule/' + Mutations.SET_COUNTRY,
       ['127.0.0.1', '192.168.1.220', '192.168.43.1'].includes(ip)
