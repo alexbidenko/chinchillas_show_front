51bcd5a1f83534b7c69c30f6bf42a6671899dc05
Alexander Bidenko <alexbidenko1998@gmail.com>
2024-03-04 07:19:14 +0300

    Update

--- plugins/userInfo.js
+++ plugins/userInfo.js
@@ -1,18 +1,17 @@
+import { getClientIp } from 'request-ip'
 import Mutations from '~/store/mutations.type'
 
-export default async ({ $cookies, $axios, store, route, redirect }) => {
-  if (process.server) {
-    if (!$cookies.get('location') && route.path !== '/loading') {
-      redirect(302, `/loading?path=${encodeURIComponent(route.path)}`)
-    } else if ($cookies.get('location')) {
-      store.commit(
-        'UserModule/' + Mutations.SET_COUNTRY,
-        $cookies.get('location')
+export default async ({ $cookies, $axios, store, req }) => {
+  if (process.server && !$cookies.get('location')) {
+    if (process.env.NODE_ENV === 'development') $cookies.set('location', 'RU')
+    else {
+      const ip = getClientIp(req)
+      const data = await $axios.$get(
+        `https://ip-info.admire.social/api/location/${ip}`
       )
+      $cookies.set('location', data.code)
     }
-  } else if (route.path === '/loading') {
-    const data = await $axios.$get('user/check')
-    $cookies.set('location', data.country)
-    location.href = route.query.path || '/'
   }
+
+  store.commit('UserModule/' + Mutations.SET_COUNTRY, $cookies.get('location'))
 }
